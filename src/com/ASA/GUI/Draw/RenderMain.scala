931544ec1cb9d3d3732b257206c4a12866d7733f
Thomas Williams <t.r.williamst@googlemail.com>
2014-11-15 00:14:33 +0000

    Added some debug code

--- src/com/ASA/GUI/Draw/RenderMain.scala
+++ src/com/ASA/GUI/Draw/RenderMain.scala
@@ -1,5 +1,7 @@
 package com.ASA.GUI.Draw
 
+import java.util.Date
+
 import com.tw10g12.Draw.Engine.{Camera, DrawTools, Colour}
 import com.tw10g12.Maths.{Vector3, Matrix4}
 
@@ -11,10 +13,11 @@ object RenderMain
     def before(drawTools: DrawTools, camera: Camera): Unit=
     {
         drawTools.clear(Colour.White)
+        drawTools.setModelView(camera.getMatrix(1.0))
         drawTools.start()
         //println("Rendering Main")
-        drawTools.setModelView(Matrix4.getIdentityMatrix)
-        drawTools.drawCuboid(new Vector3(0,0,-10), new Vector3(10.0,10.0,10.0), Array(Colour.Green))
+        //Matrix4.getIdentityMatrix.multiply(Matrix4.getTranslationMatrix(new Vector3(0,0,-100)).multiply(Matrix4.getRotationY(Math.sin(new Date().getTime()/20000.0)*180))));
+        //drawTools.drawCuboid(new Vector3(0,0,0), new Vector3(10.0,10.0,10.0), Array(Colour.Green))
 
     }
 
