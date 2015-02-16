4bf17e6ab0dcd2311008485c504f83d01ad8f939
Thomas Williams <t.r.williamst@googlemail.com>
2015-02-16 16:18:39 +0000

    Tweaked adder tiles, added editor window

--- src/com/ASA/Launcher.scala
+++ src/com/ASA/Launcher.scala
@@ -1,9 +1,9 @@
 package com.ASA
 
 import com.ASA.Controller.SimulationController
-import com.ASA.GUI.GUIWindow
+import com.ASA.GUI.{EditorWindow, SimulationWindow}
 import com.ASA.Model.ATAM.{ATAMGlue, ATAMTile}
-import com.ASA.Model.{Tile, Simulation}
+import com.ASA.Model.{Simulation, Tile}
 import com.tw10g12.Draw.Engine.Colour
 import com.tw10g12.Maths.Vector3
 
@@ -14,15 +14,20 @@ import scala.actors.threadpool.TimeUnit
  */
 object Launcher
 {
+    lazy val simulationController: SimulationController = new SimulationController(getSimulation())
+    lazy val simulationWindow: SimulationWindow = new SimulationWindow(simulationController)
+    lazy val editorWindow: EditorWindow = new EditorWindow((null, List[Tile]()))
+
     def main(args:Array[String]): Unit =
     {
-        val simulationController = new SimulationController(getSimulation())
-        val gui = new GUIWindow(simulationController)
 
-        gui.onSetup = null
-        gui.onClosed = Util.toRunnable(() => onClose(simulationController))
-        gui.startSetup()
+        simulationWindow.onSetup = null
+        simulationWindow.onClosed = Util.toRunnable(() => onClose(simulationController))
+        simulationWindow.startSetup()
 
+        editorWindow.setVisible(false)
+        editorWindow.onSetup = null
+        editorWindow.startSetup()
     }
 
     def onClose(simulationController: SimulationController): Unit =
@@ -45,8 +50,8 @@ object Launcher
             new ATAMTile(Vector(new ATAMGlue("0", 1), new ATAMGlue("1", 1), new ATAMGlue("1", 1), new ATAMGlue("0", 1)), Vector[Colour](Colour.Orange), new Vector3(0,0,0), 5)
         )
         val adderTiles = Vector[Tile](
-            new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2), new ATAMGlue("1", 1)), Vector[Colour](Colour.PleasantBlue), new Vector3(0,0,0), 0),
-            new ATAMTile(Vector(new ATAMGlue("1", 1), new ATAMGlue("W", 2), null, new ATAMGlue("W", 2)), Vector[Colour](Colour.PleasantBlue), new Vector3(0,0,0), 1),
+            new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2), new ATAMGlue("1", 1)), Vector[Colour](Colour.Green), new Vector3(0,0,0), 0),
+            new ATAMTile(Vector(new ATAMGlue("1", 1), new ATAMGlue("W", 2), null, new ATAMGlue("W", 2)), Vector[Colour](Colour.Green), new Vector3(0,0,0), 1),
             new ATAMTile(Vector(new ATAMGlue("1", 1), new ATAMGlue("0", 1), new ATAMGlue("1", 1), new ATAMGlue("0", 1)), Vector[Colour](Colour.PleasantBlue), new Vector3(0,0,0), 2),
             new ATAMTile(Vector(new ATAMGlue("1", 1), new ATAMGlue("1", 1), new ATAMGlue("0", 1), new ATAMGlue("0", 1)), Vector[Colour](Colour.PleasantBlue), new Vector3(0,0,0), 3),
             new ATAMTile(Vector(new ATAMGlue("0", 1), new ATAMGlue("0", 1), new ATAMGlue("0", 1), new ATAMGlue("0", 1)), Vector[Colour](Colour.Orange), new Vector3(0,0,0), 4),
