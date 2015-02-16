cd4fbe01688e8f864f664152aa7b8df5d897d2d2
Thomas Williams <t.r.williamst@googlemail.com>
2015-02-16 16:16:46 +0000

    Added 3D Ray for ray tracing

---
+++ src/com/tw10g12/Maths/Ray3.java
@@ -0,0 +1,31 @@
+package com.tw10g12.Maths;
+
+/**
+ * Created by Tom on 16/02/2015.
+ */
+public class Ray3
+{
+    private Vector3 rayStart;
+    private Vector3 rayDir;
+
+    public Ray3(Vector3 rayStart, Vector3 rayDir)
+    {
+        this.rayStart = rayStart;
+        this.rayDir = rayDir.normalise();
+    }
+
+    public Vector3 getRayStart()
+    {
+        return rayStart;
+    }
+
+    public Vector3 getRayDir()
+    {
+        return rayDir;
+    }
+
+    public Vector3 getPointAlongRay(double factor)
+    {
+        return rayStart.add(rayDir.multiply(factor));
+    }
+}
