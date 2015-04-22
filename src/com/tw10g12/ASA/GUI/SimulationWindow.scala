4ab2ea51128a42ed02aa5e82e5aea42e38de05ce
Thomas Williams <t.r.williamst@googlemail.com>
2015-04-22 18:20:08 +0100

    Added support for simulation tile editing
    Added support for loading and saving simulations
    Added support for state machines
    Added support for simulation statistics

--- src/com/tw10g12/ASA/GUI/SimulationWindow.scala
+++ src/com/tw10g12/ASA/GUI/SimulationWindow.scala
@@ -1,10 +1,13 @@
 package com.tw10g12.ASA.GUI
 
 import java.awt._
-import javax.swing.JPanel
+import java.util.Timer
+import javax.media.opengl.awt.GLJPanel
+import javax.swing.{JButton, JPanel}
 
-import com.tw10g12.ASA.Controller.{TilesetPanelController, SimulationController}
+import com.tw10g12.ASA.Controller.{SimulationController, TilesetPanelController}
 import com.tw10g12.ASA.GUI.DrawPanel.{DrawPanelEventHandler, SimulationDrawPanelEventHandler}
+import com.tw10g12.ASA.Model.StateMachine.StateMachine
 import com.tw10g12.ASA.Model.Tile
 import com.tw10g12.ASA.Util._
 /**
@@ -17,6 +20,15 @@ class SimulationWindow(val simulationController: SimulationController) extends A
     val CARD_SETTINGS = "settings"
 
     var tilesetPanel: TilesetPanel = null
+    var simulationPanel: SimulationPanel = null
+
+    var statsPanel: StatisticsPanel = null
+
+    var settingsPanel: SettingsPanel = null
+
+    var updateStatsTimer: Timer = new Timer()
+
+    var drawPanelEventHandler: SimulationDrawPanelEventHandler = null
 
     override def setupMenuItems(): Unit =
     {
@@ -32,47 +44,94 @@ class SimulationWindow(val simulationController: SimulationController) extends A
         setupSettingsPanel()
     }
 
+
+    override def setupDrawPanel(owningPanel: JPanel): Unit =
+    {
+        super.setupDrawPanel(owningPanel)
+
+        statsPanel = new StatisticsPanel(Vector("incorrectBindings", "stateMachines", "disabledGlues", "inertGlues"), Map("incorrectBindings" -> "Incorrect Bindings", "stateMachines" -> "State Machines", "disabledGlues" -> "Disabled Glues", "inertGlues" -> "Inert Glues"))
+        statsPanel.setup()
+        statsPanel.setVisible(false)
+
+        addToGridBag(statsPanel, owningPanel, 1, 2, 0, 0, new Insets(5, 5, 5, 5))
+
+    }
+
     override def menuSetupComplete(): Unit =
     {
         setActiveMenu(menuItems(1))
+
+        updateStatsTimer.schedule(toTimerTask(() => onStatsTimerTick), 100, 250)
+    }
+
+    def onStatsTimerTick(): Unit =
+    {
+        val simulationState = simulationController.getSimulationState()
+        if(statsPanel != null) statsPanel.update(simulationState.stats, (simulationController.getTileTypes()._1, simulationController.getTileTypes()._2.toList))
     }
 
     def setupTileSetPanel(): Unit =
     {
+        val containingPanel = new JPanel(new GridBagLayout())
         tilesetPanel = new TilesetPanel(100, 2)
-        var tileTypes = simulationController.simulation.getTileTypes()
+
+        val tileTypes = simulationController.simulation.getTileTypes()
         tilesetPanel.setTiles((tileTypes._1, tileTypes._2.toList))
         tilesetPanel.setup("Edit Tileset", new TilesetPanelController(tilesetPanel))
 
-        contentPanel.add(tilesetPanel, CARD_TILE_EDITOR)
+        val loadTilesetButton = new JButton("Load Tileset")
+        loadTilesetButton.addActionListener(tilesetPanel.controller.loadTilesetListener)
+
+        addToGridBag(tilesetPanel, containingPanel, 1, 1, 1, 1)
+        addToGridBag(loadTilesetButton, containingPanel, 1, 2, 1, 1, 0, 0, new Insets(0, 5, 5, 5))
+
+        contentPanel.add(containingPanel, CARD_TILE_EDITOR)
     }
 
     def setupSimulationPanel(): Unit =
     {
         val panel = new JPanel()
-        val simulationPanel = new SimulationPanel(simulationController)
+        simulationPanel = new SimulationPanel(simulationController)
         simulationPanel.setup()
-        simulationPanel.setBackground(Color.GREEN)
         addToGridBag(simulationPanel, panel, 1, 1, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(10, 10, 10, 10), 0, 0)
         contentPanel.add(panel, CARD_SIMULATION)
+        simulationPanel.controller.onTileTypesChanged(tilesetPanel.images)
     }
 
     def setupSettingsPanel(): Unit =
     {
-        val panel = new JPanel()
-        panel.setBackground(Color.BLUE)
-        contentPanel.add(panel, CARD_SETTINGS)
+        settingsPanel = new SettingsPanel()
+        settingsPanel.setup()
+
+        contentPanel.add(settingsPanel, CARD_SETTINGS)
     }
 
     override def getDrawPanelEventHandler(): DrawPanelEventHandler =
     {
-        return new SimulationDrawPanelEventHandler(simulationController)
+        return drawPanelEventHandler
+    }
+
+    override def getDrawPanelEventHandler(panel: GLJPanel): DrawPanelEventHandler =
+    {
+        if(drawPanelEventHandler == null)
+        {
+            drawPanelEventHandler = new SimulationDrawPanelEventHandler(simulationController, panel)
+        }
+        return drawPanelEventHandler
     }
 
     def setTileSet(tileSet: (Tile, scala.collection.immutable.List[Tile])): Unit =
     {
         tilesetPanel.setTiles(tileSet)
         simulationController.setTileTypes((tileSet._1, tileSet._2.toVector))
+        simulationPanel.controller.onTileTypesChanged(tilesetPanel.images)
         repaint()
+    }
+
+    def setStateMachines(stateMachines: Map[Tile, StateMachine]): Unit =
+    {
+        simulationController.setStateMachines(stateMachines)
+        tilesetPanel.stateMachines = stateMachines
+        simulationPanel.controller.onStateMachinesChanged(stateMachines)
     }
 }
