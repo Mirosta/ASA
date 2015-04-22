2c8e1698fb2433c56e6426c51cc8df0036bfbee1
Thomas Williams <t.r.williamst@googlemail.com>
2015-04-22 18:10:19 +0100

    Added support for state machines
    Added support for editing a paused simulation's tiles

--- src/com/tw10g12/ASA/Controller/SimulationPanelController.scala
+++ src/com/tw10g12/ASA/Controller/SimulationPanelController.scala
@@ -1,17 +1,36 @@
 package com.tw10g12.ASA.Controller
 
 import java.awt.event.{ActionEvent, ActionListener}
-import javax.swing.JButton
+import java.awt.image.BufferedImage
+import java.io.{File, FileWriter, IOException}
+import java.nio.charset.Charset
+import java.nio.file.{Files, Paths}
+import javax.swing._
+import javax.swing.border.TitledBorder
 import javax.swing.event.{ChangeEvent, ChangeListener, DocumentEvent, DocumentListener}
 
 import com.tw10g12.ASA.GUI.SimulationPanel
-import com.tw10g12.ASA.Model.{KTAMSimulation, Simulation, Tile}
+import com.tw10g12.ASA.Launcher
+import com.tw10g12.ASA.Model.JSON.JSONSimulationFactory
+import com.tw10g12.ASA.Model.StateMachine.StateMachine
+import com.tw10g12.ASA.Model.{KTAMSimulation, SMTAMSimulation, Simulation, Tile}
+import com.tw10g12.ASA.Util.IOUtil.SimulationFileFilter
+import com.tw10g12.Maths.Vector3
+import org.json.JSONObject
 
 /**
  * Created by Tom on 01/12/2014.
  */
 class SimulationPanelController(simulation: SimulationController, simulationPanel: SimulationPanel)
 {
+
+    var imageMap: scala.collection.mutable.Map[Tile, BufferedImage] = null
+    var tileIndexes: Vector[Tile] = null
+    var tileTypeIndexes: Map[Int, Int] = null
+
+    var selectedTile: Vector3 = null
+    var populating: Boolean = false
+
     val startListener = new ActionListener()
     {
         override def actionPerformed(e: ActionEvent): Unit =
@@ -20,22 +39,21 @@ class SimulationPanelController(simulation: SimulationController, simulationPane
             {
                 case button: JButton =>
                 {
-                    if (simulation.state == SimulationState.Paused || simulation.state == SimulationState.Stopped)
+                    if (simulation.state == SimulationStateEnum.Paused || simulation.state == SimulationStateEnum.Stopped)
                     {
                         val index = simulationPanel.simulationMode.getSelectedIndex
-                        if (index > -1)
+                        if (index > -1 && simulation.state == SimulationStateEnum.Stopped && simulation.needsChange())
                         {
                             val item: String = simulationPanel.simulationMode.getItemAt(index)
-                            simulation.setSimulation(getSimulation(item, simulation.getTileTypes()))
+                            simulation.moveToNextSimulation()
                         }
                         simulation.beginSimulation()
-                        button.setText("Pause")
-                        simulationPanel.stopButton.setEnabled(true)
+                        onStart()
                     }
                     else
                     {
                         simulation.pauseSimulation()
-                        button.setText("Resume")
+                        onPause()
                     }
                 }
             }
@@ -49,15 +67,27 @@ class SimulationPanelController(simulation: SimulationController, simulationPane
             {
                 case button: JButton =>
                 {
-                    if (simulation.state == SimulationState.Paused || simulation.state == SimulationState.Running)
+                    if (simulation.state == SimulationStateEnum.Paused || simulation.state == SimulationStateEnum.Running)
                     {
                         simulation.stopSimulation()
-                        button.setEnabled(false)
-                        simulationPanel.startButton.setText("Start")
+                        onStop()
                     }
                 }
             }
 
+        }
+    }
+
+    val showHideButtonListener: ActionListener = new ActionListener
+    {
+        override def actionPerformed(e: ActionEvent): Unit =
+        {
+            val visible = Launcher.simulationWindow.statsPanel.isVisible
+
+            if(visible) simulationPanel.showHideButton.setText("Show Statistics")
+            else simulationPanel.showHideButton.setText("Hide Statistics")
+
+            Launcher.simulationWindow.statsPanel.setVisible(!visible)
         }
     }
 
@@ -66,19 +96,241 @@ class SimulationPanelController(simulation: SimulationController, simulationPane
         override def actionPerformed(e: ActionEvent): Unit =
         {
             val index = simulationPanel.simulationMode.getSelectedIndex
-            if (index > -1)
+            val item: String = simulationPanel.simulationMode.getItemAt(index)
+            simulationPanel.simulationModeOptions.show(simulationPanel.simulationModeOptionsPanel, item)
+            simulationPanel.simulationModeOptionsPanel.repaint()
+
+            reloadSimulation()
+        }
+    }
+
+    def reloadSimulation(): Unit =
+    {
+        if(populating) return
+        val index = simulationPanel.simulationMode.getSelectedIndex
+        if (index > -1)
+        {
+            val item: String = simulationPanel.simulationMode.getItemAt(index)
+            simulation.setSimulation(getSimulation(item, simulation.getTileTypes()))
+            if(simulation.state == SimulationStateEnum.Stopped) simulation.moveToNextSimulation()
+        }
+    }
+
+    def onPause(): Unit =
+    {
+        simulationPanel.startButton.setText("Resume")
+        simulationPanel.stopButton.setEnabled(true)
+        simulationPanel.saveButton.setEnabled(true)
+        simulationPanel.loadButton.setEnabled(true)
+    }
+
+    def onStop(): Unit =
+    {
+        simulationPanel.startButton.setText("Start")
+        simulationPanel.stopButton.setEnabled(false)
+        simulationPanel.saveButton.setEnabled(true)
+        simulationPanel.loadButton.setEnabled(true)
+    }
+
+    def onStart(): Unit =
+    {
+        simulationPanel.startButton.setText("Pause")
+        simulationPanel.stopButton.setEnabled(true)
+        simulationPanel.saveButton.setEnabled(false)
+        simulationPanel.loadButton.setEnabled(false)
+    }
+
+    val saveSimulationListener: ActionListener = new ActionListener
+    {
+        override def actionPerformed(e: ActionEvent): Unit =
+        {
+            if(simulation.state != SimulationStateEnum.Running)
             {
-                val item: String = simulationPanel.simulationMode.getItemAt(index)
-                simulationPanel.simulationModeOptions.show(simulationPanel.simulationModeOptionsPanel, item)
-                simulationPanel.simulationModeOptionsPanel.repaint()
+                saveSimulation()
+            }
+        }
+    }
+
+    val loadSimulationListener: ActionListener = new ActionListener
+    {
+        override def actionPerformed(e: ActionEvent): Unit =
+        {
+            if(simulation.state != SimulationStateEnum.Running)
+            {
+                loadSimulation()
+            }
+        }
+    }
+
+    val changeTileListener: ActionListener = new ActionListener
+    {
+        override def actionPerformed(e: ActionEvent): Unit =
+        {
+            if(simulationPanel.changeTileTypeCombo.getSelectedIndex > -1)
+            {
+                simulation.pauseSimulation()
+                onPause()
+                val newTile = tileIndexes(simulationPanel.changeTileTypeCombo.getSelectedIndex)
+                simulation.simulation.state = simulation.simulation.state.setTile(selectedTile, newTile)
+            }
+        }
+    }
+
+    val removeTileListener: ActionListener = new ActionListener
+    {
+        override def actionPerformed(e: ActionEvent): Unit =
+        {
+            if(simulation.getSimulationState().tiles.contains(selectedTile))
+            {
+                simulation.pauseSimulation()
+                onPause()
+                simulation.simulation.state = simulation.simulation.state.setTile(selectedTile, null)
+            }
+        }
+    }
+
+    def saveSimulation(): Unit =
+    {
+        val fileChooser: JFileChooser = new JFileChooser(System.getProperty("user.home"))
+        val filter = new SimulationFileFilter
+
+        fileChooser.addChoosableFileFilter(filter)
+        fileChooser.setFileFilter(filter)
+
+        val returnVal = fileChooser.showSaveDialog(simulationPanel)
+
+        if(returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            var outputFile = fileChooser.getSelectedFile
+            if(!outputFile.getName.contains(".") && fileChooser.getFileFilter == filter)
+            {
+                outputFile = new File(outputFile.getParentFile, outputFile.getName + filter.getExtension())
+            }
+            val writer = new FileWriter(outputFile)
+            try
+            {
+                val jsonObject: JSONObject = simulation.saveSimulation()
+                writer.write(jsonObject.toString)
+            }
+            catch
+                {
+                    case ex: IOException =>
+                    {
+                        JOptionPane.showMessageDialog(simulationPanel, "Sorry this file could not be written to")
+                        Console.err.println("Couldn't write to file " + outputFile.getAbsolutePath)
+                        Console.err.println(ex.getLocalizedMessage)
+                        ex.printStackTrace()
+                    }
+                    case ex: Exception =>
+                    {
+                        JOptionPane.showMessageDialog(simulationPanel, "Unexpected error while writing to file")
+                        Console.err.println("Unexepected error while writing to file " + outputFile.getAbsolutePath)
+                        Console.err.println(ex.getLocalizedMessage)
+                        ex.printStackTrace()
+                    }
+                }
+            finally
+            {
+                writer.close()
+            }
+        }
+    }
+
+    def loadSimulation(): Unit =
+    {
+        val fileChooser: JFileChooser = new JFileChooser(System.getProperty("user.home"))
+        val filter = new SimulationFileFilter
+
+        fileChooser.addChoosableFileFilter(filter)
+        fileChooser.setFileFilter(filter)
+
+        val returnVal = fileChooser.showOpenDialog(simulationPanel)
+
+        if(returnVal == JFileChooser.APPROVE_OPTION)
+        {
+            var inputFile = fileChooser.getSelectedFile
+            if(!inputFile.exists())
+            {
+                JOptionPane.showMessageDialog(simulationPanel, "Sorry that file doesn't exist")
+                return
             }
 
+            try
+            {
+                var serialized = new JSONObject(new String(Files.readAllBytes(Paths.get(inputFile.toURI)), Charset.defaultCharset()))
+                var loadedSimulation = JSONSimulationFactory.createSimulation(serialized)
+
+                Launcher.simulationWindow.setTileSet((simulation.getTileTypes()._1, simulation.getTileTypes()._2.toList))
+                simulation.setSimulation(loadedSimulation)
+                simulation.pauseSimulation()
+                onPause()
+                populateOptions(loadedSimulation)
+            }
+            catch
+                {
+                    case ex: IOException =>
+                    {
+                        JOptionPane.showMessageDialog(simulationPanel, "Sorry this file could not be read from")
+                        Console.err.println("Couldn't write to file " + inputFile.getAbsolutePath)
+                        Console.err.println(ex.getLocalizedMessage)
+                        ex.printStackTrace()
+                    }
+                    case ex: Exception =>
+                    {
+                        JOptionPane.showMessageDialog(simulationPanel, "Unexpected error while reading from file")
+                        Console.err.println("Unexepected error while writing to file " + inputFile.getAbsolutePath)
+                        Console.err.println(ex.getLocalizedMessage)
+                        ex.printStackTrace()
+                    }
+                }
         }
+    }
+
+    def populateOptions(simulation: Simulation): Unit =
+    {
+        populating = true
+        if(simulation.isInstanceOf[KTAMSimulation])
+        {
+            val kTAMSimulation = simulation.asInstanceOf[KTAMSimulation]
+            simulationPanel.reverseConstantInput.setText(kTAMSimulation.backwardConstant.toString)
+            simulationPanel.forwardConstantInput.setText(kTAMSimulation.forwardConstant.toString)
+
+            simulationPanel.simulationMode.setSelectedItem("KTAM")
+        }
+        else if(simulation.isInstanceOf[SMTAMSimulation])
+        {
+            val sMTAMSimulation = simulation.asInstanceOf[SMTAMSimulation]
+
+            simulationPanel.checkConnected.setSelected(sMTAMSimulation.checkConnected)
+
+            simulationPanel.simulationMode.setSelectedItem("SMTAM")
+        }
+        else
+        {
+            simulationPanel.simulationMode.setSelectedItem("ATAM")
+        }
+        populating = false
+    }
+
+    def onGridClicked(gridPosition: Vector3) =
+    {
+        selectedTile = gridPosition
+        simulationPanel.simulationEditOptionsPanel.getBorder.asInstanceOf[TitledBorder].setTitle("Edit Simulation - (" + gridPosition.toString + ")" )
+        simulationPanel.simulationEditOptionsPanel.repaint()
+        simulationPanel.simulationEditOptionsPanel.setVisible(true)
+
+        if(simulation.getSimulationState().tiles.contains(gridPosition)) simulationPanel.changeTileTypeCombo.setSelectedIndex(tileTypeIndexes(simulation.getSimulationState().tiles(gridPosition).typeID))
+    }
+
+    def onGridUnselect(gridPosition: Vector3) =
+    {
+        selectedTile = null
+        simulationPanel.simulationEditOptionsPanel.setVisible(false)
     }
 
     def getSimulation(simulationType: String, tileTypes: (Tile, Vector[Tile])): Simulation =
     {
-        val functions: Map[String, Function[(Tile, Vector[Tile]), Simulation]] = Map[String, Function[(Tile, Vector[Tile]), Simulation]] ("ATAM" -> createATAM, "KTAM" -> createKTAM)
+        val functions: Map[String, Function[(Tile, Vector[Tile]), Simulation]] = Map[String, Function[(Tile, Vector[Tile]), Simulation]] ("ATAM" -> createATAM, "KTAM" -> createKTAM, "SMTAM" -> createSMTAM)
         return functions(simulationType)(tileTypes)
     }
 
@@ -89,32 +341,66 @@ class SimulationPanelController(simulation: SimulationController, simulationPane
 
     def createKTAM(tileTypes: (Tile, Vector[Tile])): Simulation =
     {
-        val reverseConstant = if(simulationPanel.reverseConstantInput.isValid()) simulationPanel.reverseConstantInput.getText.toDouble else 0.0
-        val forwardConstant = if(simulationPanel.forwardConstantInput.isValid()) simulationPanel.forwardConstantInput.getText.toDouble else 0.0
+        val reverseConstant = if(simulationPanel.reverseConstantInput.getInputVerifier.verify(simulationPanel.reverseConstantInput)) simulationPanel.reverseConstantInput.getText.toDouble else 0.0
+        val forwardConstant = if(simulationPanel.forwardConstantInput.getInputVerifier.verify(simulationPanel.forwardConstantInput)) simulationPanel.forwardConstantInput.getText.toDouble else 0.0
         return new KTAMSimulation(tileTypes._1, tileTypes._2, reverseConstant, forwardConstant)
+    }
+
+    def createSMTAM(tileTypes: (Tile, Vector[Tile])): Simulation =
+    {
+        return new SMTAMSimulation(tileTypes._1, tileTypes._2, simulation.getStateMachines(), simulationPanel.checkConnected.isSelected)
     }
 
     val KTAMConstantsChanged: DocumentListener = new DocumentListener
     {
         override def insertUpdate(e: DocumentEvent): Unit =
         {
-            onKTAMConstantsChanged()
+            onKTAMSettingsChanged()
         }
 
         override def changedUpdate(e: DocumentEvent): Unit =
         {
-            onKTAMConstantsChanged()
+            onKTAMSettingsChanged()
         }
 
         override def removeUpdate(e: DocumentEvent): Unit =
         {
-            onKTAMConstantsChanged()
+            onKTAMSettingsChanged()
+        }
+    }
+    
+    val connectedListener: ActionListener = new ActionListener
+    {
+        override def actionPerformed(e: ActionEvent): Unit =
+        {
+            onSMTAMSettingsChanged()
         }
     }
 
-    def onKTAMConstantsChanged(): Unit =
+    def onKTAMSettingsChanged(): Unit =
     {
+        reloadSimulation()
+    }
+    
+    def onSMTAMSettingsChanged(): Unit =
+    {
+        reloadSimulation()
+    }
 
+    def onTileTypesChanged(imageMap: scala.collection.mutable.Map[Tile, BufferedImage]): Unit =
+    {
+        this.imageMap = imageMap
+        this.tileIndexes = imageMap.keySet.toVector.sortBy(tile => tile.typeID)
+        this.tileTypeIndexes = (0 until tileIndexes.size).map(index => (tileIndexes(index).typeID -> index)).toMap
+        val scalingFactor = 2
+        simulationPanel.changeTileTypeCombo.setModel(new DefaultComboBoxModel[ImageIcon](tileIndexes.map(tile => new ImageIcon(imageMap(tile).getScaledInstance(imageMap(tile).getWidth / scalingFactor, imageMap(tile).getHeight / scalingFactor, java.awt.Image.SCALE_SMOOTH))).toArray))
+
+        reloadSimulation()
+    }
+
+    def onStateMachinesChanged(stateMachines: Map[Tile, StateMachine]): Unit =
+    {
+        reloadSimulation()
     }
 
     var speedChangedListener: ChangeListener = new ChangeListener
@@ -124,6 +410,5 @@ class SimulationPanelController(simulation: SimulationController, simulationPane
             simulation.setSimulationSpeed(simulationPanel.simulationSpeedSlider.getValue / 100.0)
         }
     }
-
 
 }
--- src/com/tw10g12/ASA/GUI/SimulationPanel.scala
+++ src/com/tw10g12/ASA/GUI/SimulationPanel.scala
@@ -1,8 +1,9 @@
 package com.tw10g12.ASA.GUI
 
 import java.awt.font.TextAttribute
-import java.awt.{CardLayout, GridBagLayout, Insets}
+import java.awt._
 import javax.swing._
+import javax.swing.border.{TitledBorder, LineBorder}
 
 import com.tw10g12.ASA.Controller.{SimulationController, SimulationPanelController}
 import com.tw10g12.ASA.Util
@@ -14,25 +15,44 @@ import com.tw10g12.Validation.DecimalValidator
 class SimulationPanel(simulation: SimulationController) extends JPanel(new GridBagLayout())
 {
     val controller = new SimulationPanelController(simulation, this)
+
     var startButton: JButton = null
     var stopButton: JButton = null
+    var saveButton: JButton = null
+    var loadButton: JButton = null
+    var showHideButton: JButton = null
+
     var simulationMode: JComboBox[String] = null
     var simulationModeOptions: CardLayout = null
     var simulationModeOptionsPanel: JPanel = null
+
+    var simulationSpeedSlider: JSlider = null
+
     var forwardConstantInput: JTextField = null
     var reverseConstantInput: JTextField = null
-    var simulationSpeedSlider: JSlider = null
+
+    var checkConnected: JCheckBox = null
+
+    var simulationEditOptionsPanel: JPanel = null
+    var changeTileTypeCombo: JComboBox[ImageIcon] = null
 
     def setup(): Unit =
     {
+        val upperButtonPanel = new JPanel(new GridBagLayout())
+        val lowerButtonPanel = new JPanel(new GridBagLayout())
         startButton = new JButton("Start")
         stopButton = new JButton("Stop")
+        saveButton = new JButton("<html><center>Save <p>Simulation</p></center></html>")
+        loadButton = new JButton("<html><center>Load <p>Simulation</p></center></html>")
+        showHideButton = new JButton("Show Statistics")
+
         val simulationSpeedLabel = new JLabel("Simuation Speed:")
         simulationSpeedSlider = new JSlider(1, 100)
+        simulationSpeedSlider.setBorder(new LineBorder(Color.gray))
         simulationSpeedSlider.addChangeListener(controller.speedChangedListener)
 
         val simulationModelLabel = new JLabel("Simulation Model:")
-        simulationMode = new JComboBox[String](Array[String]("ATAM", "KTAM"))
+        simulationMode = new JComboBox[String](Array[String]("ATAM", "KTAM", "SMTAM"))
         simulationMode.addActionListener(controller.modeSelectListener)
         stopButton.setEnabled(false)
 
@@ -41,17 +61,32 @@ class SimulationPanel(simulation: SimulationController) extends JPanel(new GridB
 
         simulationModeOptionsPanel.add(setupATAMOptions(), "ATAM")
         simulationModeOptionsPanel.add(setupKTAMOptions(), "KTAM")
+        simulationModeOptionsPanel.add(setupSMTAMOptions(), "SMTAM")
 
         startButton.addActionListener(controller.startListener)
         stopButton.addActionListener(controller.stopButtonListener)
+        saveButton.addActionListener(controller.saveSimulationListener)
+        loadButton.addActionListener(controller.loadSimulationListener)
+        showHideButton.addActionListener(controller.showHideButtonListener)
 
-        Util.addToGridBag(startButton, this, 1, 1, 1, 1, 0.0, 1.0, new Insets(0, 5, 0, 5))
-        Util.addToGridBag(stopButton, this, 2, 1, 1, 1, 0.0, 1.0, new Insets(0, 5, 0, 5))
+        simulationEditOptionsPanel = setupEditPanel()
+
+        Util.addToGridBag(startButton, upperButtonPanel, 1, 1, 1, 1, 0.0, 1.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 5), 0, 0)
+        Util.addToGridBag(stopButton, upperButtonPanel, 2, 1, 1, 1, 0.0, 1.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 5), 0, 0)
+        Util.addToGridBag(upperButtonPanel, this, 1, 1, 2, 1, 1.0, 1.0, new Insets(5, 0, 5, 0))
+
         Util.addToGridBag(simulationSpeedLabel, this, 1, 3, 2, 1, 0.0, 1.0, new Insets(10, 5, 0, 5))
         Util.addToGridBag(simulationSpeedSlider, this, 1, 4, 2, 1, 0.0, 1.0, new Insets(5, 5, 0, 5))
-        Util.addToGridBag(simulationModelLabel, this, 1, 5, 2, 1, 0.0, 1.0, new Insets(10, 0, 0, 0))
-        Util.addToGridBag(simulationMode, this, 1, 6, 2, 1, 0, 1.0, new Insets(5, 0, 0, 0))
-        Util.addToGridBag(simulationModeOptionsPanel, this, 1, 7, 2, 1, 0, 1.0, new Insets(5, 0, 0, 0))
+
+        Util.addToGridBag(saveButton, lowerButtonPanel, 1, 1, 1, 1, 0.0, 1.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 5), 0, 0)
+        Util.addToGridBag(loadButton, lowerButtonPanel, 2, 1, 1, 1, 0.0, 1.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 5, 0, 5), 0, 0)
+        Util.addToGridBag(showHideButton, lowerButtonPanel, 1, 2, 2, 1, 0.0, 1.0, new Insets(5, 5, 0, 5))
+        Util.addToGridBag(lowerButtonPanel, this, 1, 5, 2, 1, 1.0, 1.0, new Insets(5, 0, 0, 0))
+
+        Util.addToGridBag(simulationModelLabel, this, 1, 6, 2, 1, 0.0, 1.0, new Insets(10, 0, 0, 0))
+        Util.addToGridBag(simulationMode, this, 1, 7, 2, 1, 0, 1.0, new Insets(5, 5, 0, 5))
+        Util.addToGridBag(simulationModeOptionsPanel, this, 1, 8, 2, 1, 0, 1.0, new Insets(5, 0, 0, 0))
+        Util.addToGridBag(simulationEditOptionsPanel, this, 1, 9, 2, 1, 0, 1.0, new Insets(5, 0, 0, 0))
     }
 
     def setupATAMOptions(): JPanel =
@@ -83,6 +118,41 @@ class SimulationPanel(simulation: SimulationController) extends JPanel(new GridB
         Util.addToGridBag(forwardConstantInput, panel, 1, 2, 1, 1, 1, 0, new Insets(5, 10, 0, 5))
         Util.addToGridBag(reverseConstantLabel, panel, 1, 3, 1, 1, 1, 0, new Insets(5, 10, 0, 5))
         Util.addToGridBag(reverseConstantInput, panel, 1, 4, 1, 1, 1, 0, new Insets(5, 10, 0, 5))
+
+        return panel
+    }
+
+    def setupSMTAMOptions(): JPanel =
+    {
+        val panel = new JPanel(new GridBagLayout())
+
+        val panelLabel = new JLabel("SMTAM Options:")
+        checkConnected = new JCheckBox("Check Connected", true)
+        checkConnected.addActionListener(controller.connectedListener)
+        var fillerPanel = new JPanel()
+
+        Util.addToGridBag(panelLabel, panel, 0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 0, 0, 5), 0, 0)
+        Util.addToGridBag(checkConnected, panel, 0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL, new Insets(5, 10, 0, 5), 0, 0)
+        Util.addToGridBag(fillerPanel, panel, 0, 2, 1, 1, 1.0, 1.0)
+
+        return panel
+    }
+
+    def setupEditPanel(): JPanel =
+    {
+        val panel = new JPanel(new GridBagLayout())
+        panel.setVisible(false)
+        panel.setBorder(new TitledBorder(new LineBorder(Color.black), "Edit Simulation - (x, y, z)"))
+
+        changeTileTypeCombo = new JComboBox[ImageIcon]()
+        val changeTileButton = new JButton("Change")
+        changeTileButton.addActionListener(controller.changeTileListener)
+        val removeTileButton = new JButton("Remove")
+        removeTileButton.addActionListener(controller.removeTileListener)
+
+        Util.addToGridBag(changeTileTypeCombo, panel, 1, 1, 1, 1, 0, 1, new Insets(5, 5, 5, 5))
+        Util.addToGridBag(changeTileButton, panel, 2, 1, 1, 1, 0, 0, new Insets(5, 5, 5, 5))
+        Util.addToGridBag(removeTileButton, panel, 1, 2, 2, 1, 0, 0, new Insets(5, 5, 5, 5))
 
         return panel
     }
