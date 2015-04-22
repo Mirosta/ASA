3dbef96ea8cfe0bf47ea126aa6ad0e6cb1341537
Thomas Williams <t.r.williamst@googlemail.com>
2015-04-22 17:54:35 +0100

    Added support for using specific RNG and toJSON
    toJSON allows an entire simulation to be saved in its current state

--- src/com/tw10g12/ASA/Model/KTAMSimulation.scala
+++ src/com/tw10g12/ASA/Model/KTAMSimulation.scala
@@ -1,10 +1,14 @@
 package com.tw10g12.ASA.Model
 
+import scala.util.Random
+
 /**
  * Created by Tom on 03/03/2015.
  */
-class KTAMSimulation(startingTile: Tile, tileTypes: Vector[Tile], backwardConstant: Double, forwardConstant: Double) extends Simulation(startingTile, tileTypes)
+class KTAMSimulation(startingTile: Tile, tileTypes: Vector[Tile], val backwardConstant: Double, val forwardConstant: Double, rnd: Random) extends Simulation(startingTile, tileTypes, rnd)
 {
+    def this(startingTile: Tile, tileTypes: Vector[Tile], backwardConstant: Double, forwardConstant: Double) = this(startingTile, tileTypes, backwardConstant, forwardConstant, new Random())
+
     override def reset(): Unit =
     {
         state = new KTAMSimulationState(startingTile, tileTypes, backwardConstant, forwardConstant)
--- src/com/tw10g12/ASA/Model/Simulation.scala
+++ src/com/tw10g12/ASA/Model/Simulation.scala
@@ -1,17 +1,21 @@
 package com.tw10g12.ASA.Model
 
 import com.tw10g12.ASA.Debug.Profiler
+import com.tw10g12.ASA.Model.StateMachine.StateMachine
+import com.tw10g12.ASA.Util
+import com.tw10g12.IO.JSONSerializable
+import org.json.JSONObject
 
 import scala.util.Random
 
 /**
  * Created by Tom on 27/10/2014.
  */
-class Simulation(startingTile: Tile, tileTypes: Vector[Tile])
+class Simulation(startingTile: Tile, tileTypes: Vector[Tile], rnd: Random) extends JSONSerializable
 {
+    def this(startingTile: Tile, tileTypes: Vector[Tile]) = this(startingTile, tileTypes, new Random())
 
     var state: SimulationState = null
-    val rnd = new Random()
 
     reset()
 
@@ -29,5 +33,14 @@ class Simulation(startingTile: Tile, tileTypes: Vector[Tile])
     def getTileTypes(): (Tile, Vector[Tile]) =
     {
         return (startingTile, tileTypes)
+    }
+
+    override def toJSON(obj: JSONObject): JSONObject =
+    {
+        val stateMachines: Map[Tile, StateMachine] = if(state.isInstanceOf[SMTAMSimulationState]) state.asInstanceOf[SMTAMSimulationState].tileStateMachines else Map()
+        obj.put("rnd", Util.IOUtil.randomToJSON(rnd))
+        obj.put("tileset", Util.IOUtil.tilesetToJSON((startingTile, tileTypes.toList), stateMachines))
+        state.toJSON(obj)
+        return obj
     }
 }
