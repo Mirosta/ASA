51cbdcccb6596867c9db219d771c2afe7ea8a766
Thomas Williams <t.r.williamst@googlemail.com>
2015-04-25 15:12:45 +0100

    Added remove and update tests
    For ATAMSimulation

--- test/com/test/tw10g12/ASA/ATAMTileTest.scala
+++ test/com/test/tw10g12/ASA/ATAMTileTest.scala
@@ -11,7 +11,7 @@ import com.tw10g12.Maths.Vector3
  */
 class ATAMTileTest extends UnitSpec
 {
-    "A Simulation" should "allow a tile to be added" in
+    "An ATAM Simulation" should "allow a tile to be added" in
     {
         val seedTile = new ATAMTile(Vector(new ATAMGlue("N", 2), new ATAMGlue("E", 2), new ATAMGlue("S", 2), new ATAMGlue("W", 2)), Vector(Colour.Black), new Vector3(0,0,0), -1)
         val tileTypes = Vector(new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2)), Vector(Colour.Red), new Vector3(0,0,0), 0), new ATAMTile(Vector(new ATAMGlue("S", 2)), Vector(Colour.Black), new Vector3(), 1))
@@ -37,5 +37,54 @@ class ATAMTileTest extends UnitSpec
         setBoth.tiles(new Vector3(0, -1, 0)).typeID should be (1)
 
         setBoth.adjacencies should matchAdjacencies(1, Map(new Vector3(0,2,0)->Set(0)))
+    }
+
+    "An ATAM Simulation" should "allow a tile to be removed" in
+    {
+        val seedTile = new ATAMTile(Vector(new ATAMGlue("N", 2), new ATAMGlue("E", 2), new ATAMGlue("S", 2), new ATAMGlue("W", 2)), Vector(Colour.Black), new Vector3(0,0,0), -1)
+        val tileTypes = Vector(new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2)), Vector(Colour.Red), new Vector3(0,0,0), 0), new ATAMTile(Vector(new ATAMGlue("S", 2)), Vector(Colour.Black), new Vector3(), 1))
+
+        val simulation = new SimulationState(seedTile, tileTypes).setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1))
+
+        val removeTileAbove = simulation.setTile(new Vector3(0, 1, 0), null)
+        val removeTileBelow = simulation.setTile(new Vector3(0, -1, 0), null)
+        val removeBoth = simulation.setTile(new Vector3(0, 1, 0), null).setTile(new Vector3(0, -1, 0), null)
+
+        removeTileAbove.tiles.contains(new Vector3(0, 1, 0)) should be (false)
+        removeTileAbove.adjacencies should matchAdjacencies(1, Map(new Vector3(0,1,0)->Set(0)))
+
+        removeTileBelow.tiles.contains(new Vector3(0, -1, 0)) should be (false)
+        removeTileBelow.adjacencies should matchAdjacencies(1, Map(new Vector3(0,-1,0)->Set(1)))
+
+        removeBoth.tiles.contains(new Vector3(0, 1, 0)) should be (false)
+        removeBoth.tiles.contains(new Vector3(0, -1, 0)) should be (false)
+
+        removeBoth.adjacencies should matchAdjacencies(2, Map(new Vector3(0,1,0)->Set(0), new Vector3(0, -1, 0) -> Set(1)))
+    }
+
+    "An ATAM Simulation" should "allow a tile to be updated" in
+    {
+        val seedTile = new ATAMTile(Vector(new ATAMGlue("N", 2), new ATAMGlue("E", 2), new ATAMGlue("S", 2), new ATAMGlue("W", 2)), Vector(Colour.Black), new Vector3(0,0,0), -1)
+        val tileTypes = Vector(new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2)), Vector(Colour.Red), new Vector3(0,0,0), 0), new ATAMTile(Vector(new ATAMGlue("S", 2)), Vector(Colour.Black), new Vector3(), 1), new ATAMTile(Vector(new ATAMGlue("S", 2), null, new ATAMGlue("S", 2)), Vector(Colour.Red), new Vector3(0,0,0), 2), new ATAMTile(Vector(null, null, new ATAMGlue("N", 2)), Vector(Colour.Black), new Vector3(), 3))
+
+        val simulation = new SimulationState(seedTile, tileTypes).setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1))
+
+        val terminateTileAbove = simulation.setTile(new Vector3(0, 1, 0), tileTypes(3))
+        val chainTileBelow = simulation.setTile(new Vector3(0, -1, 0), tileTypes(2))
+        val updateBoth = simulation.setTile(new Vector3(0, 1, 0), tileTypes(3)).setTile(new Vector3(0, -1, 0), tileTypes(2))
+
+        terminateTileAbove.tiles.contains(new Vector3(0, 1, 0)) should be (true)
+        terminateTileAbove.tiles(new Vector3(0, 1, 0)).typeID should be (3)
+        terminateTileAbove.adjacencies should matchAdjacencies(0, Map())
+
+        chainTileBelow.tiles.contains(new Vector3(0, -1, 0)) should be (false)
+        chainTileBelow.tiles(new Vector3(0, -1, 0)).typeID should be (2)
+        chainTileBelow.adjacencies should matchAdjacencies(2, Map(new Vector3(0,-2,0)->Set(1, 2)))
+
+        updateBoth.tiles.contains(new Vector3(0, 1, 0)) should be (true)
+        updateBoth.tiles(new Vector3(0, 1, 0)).typeID should be (3)
+        updateBoth.tiles.contains(new Vector3(0, -1, 0)) should be (true)
+        updateBoth.tiles(new Vector3(0, -1, 0)).typeID should be (2)
+        updateBoth.adjacencies should matchAdjacencies(1, Map(new Vector3(0,-2,0)->Set(1, 2)))
     }
 }
