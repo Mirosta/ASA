19f0d3d0c2c0ae7e5d121475d28fd65b065d4360
Thomas Williams <t.r.williamst@googlemail.com>
2015-04-25 16:12:07 +0100

    First stab at SMTAM tests

--- test/com/test/tw10g12/ASA/SMTAMTileTest.scala
+++ test/com/test/tw10g12/ASA/SMTAMTileTest.scala
@@ -2,7 +2,7 @@ package com.test.tw10g12.ASA
 
 import com.test.tw10g12.Test.UnitSpec
 import com.tw10g12.ASA.Model.ATAM.{ATAMGlue, ATAMTile}
-import com.tw10g12.ASA.Model.SimulationState
+import com.tw10g12.ASA.Model.SMTAMSimulationState
 import com.tw10g12.Draw.Engine.Colour
 import com.tw10g12.Maths.Vector3
 
@@ -15,11 +15,11 @@ class SMTAMTileTest extends UnitSpec
         {
             val seedTile = new ATAMTile(Vector(new ATAMGlue("N", 2), new ATAMGlue("E", 2), new ATAMGlue("S", 2), new ATAMGlue("W", 2)), Vector(Colour.Black), new Vector3(0,0,0), -1)
             val tileTypes = Vector(new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2)), Vector(Colour.Red), new Vector3(0,0,0), 0), new ATAMTile(Vector(new ATAMGlue("S", 2)), Vector(Colour.Black), new Vector3(), 1))
-            val simulation = new SimulationState(seedTile, tileTypes)
+            val simulation = new SMTAMSimulationState(seedTile, tileTypes, Map(), true)
 
-            val setTileAbove = simulation.setTile(new Vector3(0, 1, 0), tileTypes(0))
-            val setTileBelow = simulation.setTile(new Vector3(0, -1, 0), tileTypes(1))
-            val setBoth = simulation.setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1))
+            val setTileAbove = simulation.setTile(new Vector3(0, 1, 0), tileTypes(0)).asInstanceOf[SMTAMSimulationState]
+            val setTileBelow = simulation.setTile(new Vector3(0, -1, 0), tileTypes(1)).asInstanceOf[SMTAMSimulationState]
+            val setBoth = simulation.setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1)).asInstanceOf[SMTAMSimulationState]
 
             setTileAbove.tiles.contains(new Vector3(0, 1, 0)) should be (true)
             setTileAbove.tiles(new Vector3(0, 1, 0)).typeID should be (0)
@@ -44,11 +44,11 @@ class SMTAMTileTest extends UnitSpec
             val seedTile = new ATAMTile(Vector(new ATAMGlue("N", 2), new ATAMGlue("E", 2), new ATAMGlue("S", 2), new ATAMGlue("W", 2)), Vector(Colour.Black), new Vector3(0,0,0), -1)
             val tileTypes = Vector(new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2)), Vector(Colour.Red), new Vector3(0,0,0), 0), new ATAMTile(Vector(new ATAMGlue("S", 2)), Vector(Colour.Black), new Vector3(), 1))
 
-            val simulation = new SimulationState(seedTile, tileTypes).setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1))
+            val simulation = new SMTAMSimulationState(seedTile, tileTypes, Map(), true).setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1))
 
-            val removeTileAbove = simulation.setTile(new Vector3(0, 1, 0), null)
-            val removeTileBelow = simulation.setTile(new Vector3(0, -1, 0), null)
-            val removeBoth = simulation.setTile(new Vector3(0, 1, 0), null).setTile(new Vector3(0, -1, 0), null)
+            val removeTileAbove = simulation.setTile(new Vector3(0, 1, 0), null).asInstanceOf[SMTAMSimulationState]
+            val removeTileBelow = simulation.setTile(new Vector3(0, -1, 0), null).asInstanceOf[SMTAMSimulationState]
+            val removeBoth = simulation.setTile(new Vector3(0, 1, 0), null).setTile(new Vector3(0, -1, 0), null).asInstanceOf[SMTAMSimulationState]
 
             removeTileAbove.tiles.contains(new Vector3(0, 1, 0)) should be (false)
             removeTileAbove.adjacencies should matchAdjacencies(1, Map(new Vector3(0,1,0)->Set(0)))
@@ -67,11 +67,11 @@ class SMTAMTileTest extends UnitSpec
             val seedTile = new ATAMTile(Vector(new ATAMGlue("N", 2), new ATAMGlue("E", 2), new ATAMGlue("S", 2), new ATAMGlue("W", 2)), Vector(Colour.Black), new Vector3(0,0,0), -1)
             val tileTypes = Vector(new ATAMTile(Vector(new ATAMGlue("N", 2), null, new ATAMGlue("N", 2)), Vector(Colour.Red), new Vector3(0,0,0), 0), new ATAMTile(Vector(new ATAMGlue("S", 2)), Vector(Colour.Black), new Vector3(), 1), new ATAMTile(Vector(new ATAMGlue("S", 2), null, new ATAMGlue("S", 2)), Vector(Colour.Red), new Vector3(0,0,0), 2), new ATAMTile(Vector(null, null, new ATAMGlue("N", 2)), Vector(Colour.Black), new Vector3(), 3))
 
-            val simulation = new SimulationState(seedTile, tileTypes).setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1))
+            val simulation = new SMTAMSimulationState(seedTile, tileTypes, Map(), true).setTile(new Vector3(0, 1, 0), tileTypes(0)).setTile(new Vector3(0, -1, 0), tileTypes(1))
 
-            val terminateTileAbove = simulation.setTile(new Vector3(0, 1, 0), tileTypes(3))
-            val chainTileBelow = simulation.setTile(new Vector3(0, -1, 0), tileTypes(2))
-            val updateBoth = simulation.setTile(new Vector3(0, 1, 0), tileTypes(3)).setTile(new Vector3(0, -1, 0), tileTypes(2))
+            val terminateTileAbove = simulation.setTile(new Vector3(0, 1, 0), tileTypes(3)).asInstanceOf[SMTAMSimulationState]
+            val chainTileBelow = simulation.setTile(new Vector3(0, -1, 0), tileTypes(2)).asInstanceOf[SMTAMSimulationState]
+            val updateBoth = simulation.setTile(new Vector3(0, 1, 0), tileTypes(3)).setTile(new Vector3(0, -1, 0), tileTypes(2)).asInstanceOf[SMTAMSimulationState]
 
             terminateTileAbove.tiles.contains(new Vector3(0, 1, 0)) should be (true)
             terminateTileAbove.tiles(new Vector3(0, 1, 0)).typeID should be (3)
